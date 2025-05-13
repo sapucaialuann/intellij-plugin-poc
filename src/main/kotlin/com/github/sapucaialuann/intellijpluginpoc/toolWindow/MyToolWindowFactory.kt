ed8d9961e9b2f2cf6f241364122a7e0f40039ce0
GitHub Action <action@github.com>
2025-05-13 11:10:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/sapucaialuann/intellijpluginpoc/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sapucaialuann/intellijpluginpoc/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sapucaialuann.intellijpluginpoc.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sapucaialuann.intellijpluginpoc.MyBundle
+import com.github.sapucaialuann.intellijpluginpoc.services.MyProjectService
 import javax.swing.JButton
 
 
